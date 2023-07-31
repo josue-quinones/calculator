b80f905f253277e2ba838d758ca54269da23ac83
Josue Quinones <74983067+josue-quinones@users.noreply.github.com>
2023-07-31 15:10:30 -0400

    Added buttons to hmtl, basic functions to script

---
+++ script.js
@@ -0,0 +1,32 @@
+var operand1,operand2,operator;
+
+function add(n,m) {
+    return n + m;
+}
+
+function substract(n,m) {
+    return n - m;
+}
+
+function multiply(n,m) {
+    return n * m;
+}
+
+function divide(n,m) {
+    return n / m;
+}
+
+function operate(operator,op1,op2) {
+    switch(operator) {
+        case "+":
+            return add(op1,op2);
+        case "-":
+            return substract(op1,op2);
+        case "x":
+            return multiply(op1,op2);
+        case "\/":
+            return divide(op1,op2);
+        default:
+            break;
+    }
+}
